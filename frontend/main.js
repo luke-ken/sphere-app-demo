5279d7891a9ba1f053d14b7d991889dd2b75899d
tuanpham <tuan.pham.se@gmail.com>
2024-11-05 21:23:25 +0100

    feat(frontend): add the input field to set the sphere's radius

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-
+import {v4 as uuidv4} from 'uuid';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -10,7 +10,6 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById('app').appendChild(renderer.domElement);
 
 let sphere;
-let radius = 1;
 
 // create and render the sphere
 function createSphere(radius) {
@@ -31,5 +30,59 @@ function animate() {
     renderer.render(scene, camera);
 }
 
-createSphere(radius);
+// fetch the stored radius from backend and render the sphere
+async function fetchRadiusAndCreateSphere() {
+    const id = uuidv4()
+    try {
+        const response = await fetch("http://localhost:9003/api/json-rpc-sphere", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                jsonrpc: "2.0",
+                method: "getRadius",
+                id: id
+            })
+        })
+        const data = await response.json();
+        const radius = data.result;
+        createSphere(radius);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function setRadius(newRadius) {
+    const id = uuidv4()
+    try {
+        await fetch("http://localhost:9003/api/json-rpc-sphere", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                jsonrpc: "2.0",
+                method: "setRadius",
+                params: {radius: newRadius},
+                id: id,
+            })
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// attach event listener to submit button
+document.getElementById("submitRadius").addEventListener("click", () => {
+    const radiusInput = document.getElementById("radiusInput").value;
+    if (radiusInput) {
+        setRadius(parseFloat(radiusInput))
+            .then(() => fetchRadiusAndCreateSphere()) // fetch newly updated radius after setRadius
+            .catch(error => console.log(error));
+    }
+});
+
+// entry point of the app
+fetchRadiusAndCreateSphere().catch(error => console.log(error));
 animate();
